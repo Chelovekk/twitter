7fa409e8d6e4900be1e8bde8591d643ac59ce4f8
vadym <vadym.toporovskyi@sapient.pro>
2022-07-08 17:56:54 +0300

    enviroment changed

--- src/webhooks/webhook-activity.service.ts
+++ src/webhooks/webhook-activity.service.ts
@@ -8,16 +8,16 @@ dotenv.config()
 @Injectable()
 export class WebhookActivityService {
   async handle() {
-    const a = userActivity({
-      serverUrl: process.env.BASE_URL,
-      route: '/webhook/twitter',
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      accessToken: process.env.TWITTER_ACCESS_TOKEN,
-      accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-      environment: 'AccountActivity'
-    })
-    console.log(a)
+    // const a = userActivity({
+    //   serverUrl: process.env.BASE_URL,
+    //   route: '/webhook/twitter',
+    //   consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    //   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    //   accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    //   accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    //   environment: 'webhooks'
+    // })
+    // console.log(a)
     return await userActivity({
       serverUrl: process.env.BASE_URL,
       route: '/webhook/twitter',
@@ -25,7 +25,7 @@ export class WebhookActivityService {
       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
       accessToken: process.env.TWITTER_ACCESS_TOKEN,
       accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-      environment: 'AccountActivity'
+      environment: 'webhooks'
     })
   }
 }
